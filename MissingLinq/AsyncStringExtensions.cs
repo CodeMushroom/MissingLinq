4f711ce62e53fe0cf95ce47c72bfc158a6693a4d
Terry Tice <nufyoot@gmail.com>
2015-08-07 20:36:33 -0400

    New features
    - Added "Contains" string extension to accept StringComparison
    - Added "ContainsAsync" for Task<string> types.

---
+++ MissingLinq/AsyncStringExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MissingLinq
+{
+    /// <summary>
+    /// Provides a set of extension methods for operations against Task&lt;string&gt;
+    /// </summary>
+    public static class AsyncStringExtensions
+    {
+        /// <summary>
+        /// Determines whether or not the current string contains the specified value.
+        /// </summary>
+        /// <param name="input">The string to be checked once the task has finished.</param>
+        /// <param name="value">The value to check the string for.</param>
+        /// <returns>Returns true if the current string contains the provided value.</returns>
+        public static async Task<bool> ContainsAsync(this Task<string> task, string value)
+        {
+            return (await task).Contains(value);
+        }
+
+        /// <summary>
+        /// Determines whether or not the current string contains the specified value.
+        /// </summary>
+        /// <param name="input">The string to be checked once the task has finished.</param>
+        /// <param name="value">The value to check the string for.</param>
+        /// <param name="comparisonType">The string comparison type to be used for this contains operation.</param>
+        /// <returns>Returns true if the current string contains the provided value.</returns>
+        public static async Task<bool> ContainsAsync(this Task<string> task, string value, StringComparison compareType)
+        {
+            return (await task).Contains(value, compareType);
+        }
+    }
+}
--- MissingLinq/StringExtensions.cs
+++ MissingLinq/StringExtensions.cs
@@ -12,6 +12,18 @@ namespace MissingLinq
     public static class StringExtensions
     {
         /// <summary>
+        /// Determines whether or not the current string contains the specified value.
+        /// </summary>
+        /// <param name="input">The string to be checked.</param>
+        /// <param name="value">The value to check the string for.</param>
+        /// <param name="comparisonType">The string comparison type to be used for this contains operation.</param>
+        /// <returns>Returns true if the current string contains the provided value.</returns>
+        public static bool Contains(this string input, string value, StringComparison comparisonType)
+        {
+            return input.IndexOf(value, comparisonType) != -1;
+        }
+
+        /// <summary>
         /// Determines is a string is null or empty.
         /// </summary>
         /// <param name="input">The string to be checked.</param>
